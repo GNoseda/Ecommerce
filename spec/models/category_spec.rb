d4513e90fa3d2113ebc1305e0b1e3f895af02924
GNoseda <g.noseda.soto@gmail.com>
2021-02-01 20:11:48 -0300

    about to try major modifications to schema

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  it 'Brings all ancestors categories' do
+    category_1 = Category.create(name:'category_01')
+    category_2 = Category.create(name:'category_02', category_id:1)
+    expect(category_2.ancestry).to  eq([1])
+  end
+
+  if 'Brings all offsprings categories' do
+    category_1 = Category.create(name:'category_01')
+    category_2 = Category.create(name:'category_02', category_id:1)
+    expect(category_1.ancestry).to  eq([2])
+  end
+end
